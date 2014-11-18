9cbb907b97143ce77417e1215d5669bd450fdb26
Paul P <paulp@hotmail.sk>
2014-11-18 16:55:40 +0100

    Computing GCD in Functional programming

---
+++ Hackerrank/practice/fp/recursion/ComputingGcd.scala
@@ -0,0 +1,24 @@
+package fp.recursion
+
+/**
+ * @author Pidanic
+ */
+object ComputingGcd {
+    
+   def gcd(x: Int, y: Int): Int =
+   {
+     if(y == 0) x else gcd(y, x % y)
+   }
+  
+
+   /**This part handles the input/output. Do not change or modify it **/
+  def acceptInputAndComputeGCD(pair:List[Int]) = {
+      println(gcd(pair.head,pair.reverse.head))
+  } 
+
+    def main(args: Array[String]) {
+/** The part relates to the input/output. Do not change or modify it **/
+         acceptInputAndComputeGCD(readLine().trim().split(" ").map(x=>x.toInt).toList)
+
+    }
+}
