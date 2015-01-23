f451609cc5f76855828feb47c228ca329b117b2e
paulp <paulp@hotmail.sk>
2015-01-23 19:11:25 +0100

    bash tutorial - A personalized echo

---
+++ practice/linux_shell/bash_tutorials/a_personalized_echo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Write a bash script which accepts as input, a name, and displays a greeting: "Welcome (name)"
+#
+# Input Format 
+# One line, containing a name
+#
+# Output Format 
+# One line: "Welcome (name)" (quotation marks excluded) 
+# The evaluation will be case sensitive.
+
+read name
+echo "Welcome $name"
