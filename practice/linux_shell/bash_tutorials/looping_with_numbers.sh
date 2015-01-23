b798e7f902eb4b15c35600fb5a047c88d93a496d
paulp <paulp@hotmail.sk>
2015-01-23 19:15:19 +0100

    bash tutorials - looping with numbers

---
+++ practice/linux_shell/bash_tutorials/looping_with_numbers.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# For loops in Bash can be used in several ways: - iterating between two integers a and b - iterating between two integer a and b and incrementing by c each time - iterating through the elements of an array, etc.
+# 
+# Use for loops to display the natural numbers 1 to 50.
+
+for i in {1..50} 
+do
+  echo $i
+done
