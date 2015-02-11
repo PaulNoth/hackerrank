d555c218f1efb5db6765c844bea50d83a09f8842
paulp <paulp@hotmail.sk>
2015-02-11 09:13:57 +0100

    bash tutorials - sort command

---
+++ practice/linux_shell/bash_tutorials/the_sort_command1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Given a file of text, re-order the lines in lexicographical order.
+
+sort -n
---
+++ practice/linux_shell/bash_tutorials/the_sort_command2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Given a file of text, re-order the lines in reverse lexicographical order (i.e. Z-A instead of A-Z).
+
+sort -r
