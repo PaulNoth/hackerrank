7977711059e2edbf19bbe2dab65fe8af63cbabf1
Paul P <paulp@hotmail.sk>
2015-07-08 20:38:14 +0200

    A very big sum added

---
+++ practice/algorithms/warmup/AVeryBigSum.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class AVeryBigSum {
+
+    public static void main(String[] args) {
+        Scanner stdin = new Scanner(System.in);
+        int numbers = Integer.parseInt(stdin.nextLine());
+        String[] integers = stdin.nextLine().split(" ");
+        long sum = 0;
+        for(int i = 0; i < numbers; i++)
+        {
+            sum += Long.parseLong(integers[i]);
+        }
+        System.out.println(sum);
+        stdin.close();
+    }
+}
