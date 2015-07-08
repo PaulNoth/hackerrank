56abc9cb097e2311acddb32158f221939c778082
Paul P <paulp@hotmail.sk>
2015-07-08 20:52:18 +0200

    Prefix Compression added

---
+++ practice/fp/recursion/PrefixCompression.scala
@@ -0,0 +1,39 @@
+package fp.recursion
+
+/**
+ * @author Pavol
+ */
+object PrefixCompression extends App {
+//  val a = "puppy"
+//  val b = "puppy"
+  
+  val lines = io.Source.stdin.getLines.toArray
+  val a = lines(0)
+  val b = lines(1)
+  val prefix = commonPrefix(a, b, 0)
+  
+  println(prefix + " " + a.substring(0, prefix))
+  println((a.length - prefix) + " " + a.substring(prefix))
+  println((b.length - prefix) + " " + b.substring(prefix))
+  
+  private def commonPrefix(a : String, b: String, index: Int): Int = 
+  {
+    if(index < 0)
+    {
+      return 0
+    }
+    if(index == a.length)
+    {
+      return index
+    }
+    if(index == b.length)
+    {
+      return index
+    }
+    if(a.charAt(index) == b.charAt(index))
+    {
+      return commonPrefix(a, b, index + 1);
+    }
+    return index;
+  }
+}
