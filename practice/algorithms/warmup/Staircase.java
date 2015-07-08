95185bbb3fff4edcf3380d480a58a681b040b89e
Paul P <paulp@hotmail.sk>
2015-07-08 20:56:30 +0200

    Staircase added

---
+++ practice/algorithms/warmup/Staircase.java
@@ -0,0 +1,33 @@
+package algorithms.warmup;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Staircase 
+{
+
+    public static void main(String[] args) 
+	{
+        Scanner stdin = new Scanner(System.in);
+        int height = Integer.parseInt(stdin.nextLine());
+        for(int i = 0; i < height; i++)
+        {
+           for(int j = 0; j < height; j++)
+           {
+                if(height - i - 2 < j)
+                {
+                    System.out.print("#");
+                }
+               else
+               {
+                   System.out.print(" ");
+               }
+           }
+            System.out.println();
+        }
+        stdin.close();
+    }
+}
