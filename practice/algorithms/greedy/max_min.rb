af48f6dcab463ffe592b51ac654e0f5ab5f5023f
Paul P <paulp@hotmail.sk>
2015-07-08 21:15:08 +0200

    Max Min added

---
+++ practice/algorithms/greedy/max_min.rb
@@ -0,0 +1,13 @@
+n = gets.to_i
+k = gets.to_i
+candy = Array.new(n)
+for i in 0..n-1
+      candy[i] = gets.to_i
+end
+candy.sort! {|l,j| l <=> j}
+unfairness = candy[candy.length - 1]
+for i in 0..candy.length - k do
+  temp_unfairness = candy[i + k - 1] - candy[i]
+  unfairness = temp_unfairness if unfairness > temp_unfairness  
+end
+puts unfairness
