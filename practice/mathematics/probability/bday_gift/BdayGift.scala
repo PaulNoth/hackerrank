7035d5e55945497c59895e5995eee262756860f8
Paul P <paulp@hotmail.sk>
2015-12-25 12:37:09 +0100

    fix

--- practice/mathematics/probability/bday_gift/BdayGift.scala
+++ practice/mathematics/probability/bday_gift/BdayGift.scala
@@ -2,5 +2,5 @@ import scala.io.Source
 
 object BdayGift extends App {
   val lines = Source.stdin.getLines().drop(1).map(_.toLong)
-  printf("%.1lf", lines.sum.toDouble / 2)
+  printf("%.1f", lines.sum.toDouble / 2)
 }
