0e9fb00617c440c30cd5120e9f181fb206f3ddc5
Paul P <paulp@hotmail.sk>
2016-01-04 10:51:04 +0100

    Snakes and Ladders: The Quickest Way Up solution

--- practice/algorithms/graphtheory/snakes_and_ladders_the_quickest_way_up/snakes_ladders.rb
+++ practice/algorithms/graphtheory/snakes_and_ladders_the_quickest_way_up/snakes_ladders.rb
@@ -1,12 +1,3 @@
-# Riesenie:
-# - Prechadzam policko po policku, ci sa nenachadza rebrik.
-# - Ak ano vytvorim novy objekt, ktory bude obsahovat dosiahnutu poziciu a pocet hodeni kocky
-# a pridam ho do frontu tahov
-# inak sa posuniem vzdy o 6 a tento posun vlozim do frontu
-# (tak prechadzam strom rieseni s co navacsimi posunmi)
-# - robim tak dovtedy, kym nedosiahnem poziciu 100 a viac
-# - potom vyberiem ten podstrom, ktorym som dosiahol najmenej hodov
-
 class Move
   attr_reader :rolls, :position
   
@@ -17,8 +8,7 @@ class Move
 end
 
 test_cases = gets.chomp.to_i
-test_cases.times do 
-  # spracuje input
+test_cases.times do
   (ladders_count, snakes_count) = gets.chomp.split(",").map { |e| e.to_i }
   ladders = gets.chomp.split
   snakes = gets.chomp.split
@@ -34,15 +24,12 @@ test_cases.times do
     snakes_hash[start_p] = end_p
   end
   
-  
-  
   start = Move.new(1, 0)
   moves = []
-  moves.push(start) # inicializujem 1. tah
-  rolls_at_hundred = [] # zodpoveda poctu hodov kocky jedneho podstromu pri dosiahnuti policka 100
+  moves.push(start)
+  rolls_at_hundred = []
   while !moves.empty? do
     move = moves.shift
-   # puts "Move poisition: #{move.position}, rolls: #{move.rolls}"
     actual_position = move.position
     actual_rolls = move.rolls
     
@@ -68,7 +55,7 @@ test_cases.times do
       moves.push(new_move)
     end
   end
-  # usporiadam tahy a vyberiem najmensi
+
   rolls_at_hundred.sort! { |k, l| k <=> l}
   puts rolls_at_hundred[0]
 end
