6d3b74e576c0fc630800420ccfe467b53fcccef5
Pavol Pidanič <paulp@hotmail.sk>
2016-01-22 16:52:29 +0100

    Java If-Else solution

---
+++ practice/java/introduction/java_if-else/JavaIfElse.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class JavaIfElse {
+   public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();            
+       String ans = "";
+       if(n % 2 == 1) {
+           ans = "Weird";
+       }
+       else {
+           if(n >= 2 && n <= 5) {
+               ans = "Not Weird";
+           } else if(n >= 6 && n <= 20) {
+               ans = "Weird";
+           } else {
+               ans = "Not Weird";
+           }
+       }
+       System.out.println(ans);
+   }
+}
