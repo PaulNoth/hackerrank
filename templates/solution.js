be9679ac7e48f35e974d5649a36b75bbbe428c6a
Pavol Pidanič <paul@hotmail.sk>
2016-04-15 16:31:03 +0200

    updated variable declarations

--- templates/solution.js
+++ templates/solution.js
@@ -1,13 +1,13 @@
 'use strict';
 
 const processData = input => {
-    let lines = input.split('\n');
+    const lines = input.split('\n');
     // code goes here
 };
 
 process.stdin.resume();
 process.stdin.setEncoding("ascii");
 
-var _input = "";
+let _input = "";
 process.stdin.on("data", input => _input += input);
 process.stdin.on("end", () => processData(_input));
