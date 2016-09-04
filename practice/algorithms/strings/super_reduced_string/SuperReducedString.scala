9ab627d49d7fced85efb43a02c04ccb154c2e1d2
Paul P <paulp@hotmail.sk>
2016-09-04 13:23:15 +0200

    Super Reduced String solution in scala

---
+++ practice/algorithms/strings/super_reduced_string/SuperReducedString.scala
@@ -0,0 +1,11 @@
+import scala.io.Source
+
+object SuperReducedString extends App {
+  val line = Source.stdin.getLines().toList.head
+  val reduced = line.foldLeft("")(stringReduce)
+  println(if(reduced.isEmpty) "Empty String" else reduced)
+
+  def stringReduce(acc: String, c: Char): String = {
+    if(acc.charAt(acc.length - 1) == c) acc.substring(0, acc.length - 1) else acc + c
+  }
+}
