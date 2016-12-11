50d67365e4d31085adc26b89cab5d8c82ad1f2a0
Paul P <paulp@hotmail.sk>
2016-12-11 15:21:18 +0100

    Sum vs Xor solution in scala

---
+++ practice/algorithms/bit_manipulation/sum_vs_xor/SumVsXor.scala
@@ -0,0 +1,6 @@
+object SumVsXor extends App {
+  val n = Source.stdin.bufferedReader().readLine().toLong
+  val satisfied = Math.pow(2L, if(n == 0L) 0 else n.toBinaryString.count(_ == '0')).toLong
+
+  println(satisfied)
+}
